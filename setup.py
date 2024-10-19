f8f89b42d5cccfb3eab82412ed7e38848c976ec9
datasint <gromovvsint@proton.me>
2024-10-19 12:30:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dorksint',
-    version='1.0.0',
+    version='1.0.5',
     packages=find_packages(),
     py_modules=['dorksint'],
     install_requires=[
