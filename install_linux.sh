8b982d196795dfb5a305ef82d934ee1b7015516d
datasint <gromovvsint@proton.me>
2024-10-23 20:30:08 +0300

    Added installation files for Windows and Linux sys

---
+++ install_linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+pip install .
+
+
+echo "Installed!"
+echo "See https://github.com/datasint/DorkSint?tab=readme-ov-file#%EF%B8%8F-usage."
---
+++ install_windows.bat
@@ -0,0 +1,11 @@
+@echo off
+
+
+pip install -r requirements.txt
+pip install .
+
+echo Installed!
+echo See https://github.com/datasint/DorkSint?tab=readme-ov-file#%EF%B8%8F-usage.
+
+pause
+exit
